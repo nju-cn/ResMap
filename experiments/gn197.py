3dfd2666d38bdb5879d0cedae1dfbca6a511ba9f
ZhangShuai <1290645535@qq.com>
2021-12-22 19:59:51 +0800

    [实验] 修改了gn197.py中读取的LFCNZ数据名称，使之与当前命名兼容

--- experiments/gn197.py
+++ experiments/gn197.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
-    with open(f'dataset/road.gn.lfcnz', 'rb') as f:
+    with open(f'dataset/gn.road.480x720.400.lfcnz', 'rb') as f:
         lfcnz = pickle.load(f)
     x_total = [sum(lfcnz[196][f]) / len(lfcnz[196][f]) for f in range(1, len(lfcnz[196]))]
     y_total = [sum(lfcnz[197][f]) / len(lfcnz[197][f]) for f in range(1, len(lfcnz[197]))]
