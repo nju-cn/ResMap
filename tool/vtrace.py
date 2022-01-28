16d05b2ac781f03079d6f150b521ef9e6888707f
ZhangShuai <1290645535@qq.com>
2022-01-28 11:58:03 +0800

    [实验] vtrace代码整理，使用3种模式读取tc文件。3种模式测试都正常

--- tool/vtrace.py
+++ tool/vtrace.py
@@ -1,7 +1,11 @@
+import glob
+import os
+import shutil
+import zipfile
 from datetime import datetime
 from dataclasses import dataclass
-import os
-from typing import List, Dict, Tuple, Optional
+from io import TextIOWrapper
+from typing import List, Dict, Tuple, Optional, TextIO, Union
 
 from matplotlib import pyplot as plt
 import matplotlib.colors as mcolors
@@ -18,16 +22,18 @@ class Event:
     is_start: bool  # start还是finish
 
 
-def read_events(filename: str, ifr_num: int = -1) -> List[List[Event]]:
+def read_events(tcfile: Union[str, TextIO], ifr_num: int = -1) -> List[List[Event]]:
     """从某个设备生成的tc文件中读取事件
-    :param filename tc文件路径
+    :param tcfile tc文件名，或者file-like对象
     :param ifr_num 所有事件中的IFR数量(id从0计数到ifr_num-1)。若未知可不填
     :return i_evts i_evts[i]对应id=i的IFR所有事件，事件按照时间顺序排序
     """
     events = []
     ifr_cnt = 0
-    with open(filename, 'r') as file:
-        for line in file:
+    if isinstance(tcfile, str):
+        tcfile = open(tcfile, 'r')
+    with tcfile:
+        for line in tcfile:
             timestamp, is_start, act, ifr = line[:-1].split(' ')
             timestamp = datetime.fromisoformat(timestamp)
             is_start = (is_start == 'start')
@@ -48,6 +54,15 @@ class Stage:
     start: datetime  # 起始时间
     finish: datetime  # 结束时间
 
+    def __repr__(self):
+        return f"Stage('{self.trans_thread(self.thread)}':{self.act}," \
+               f"s='{self.start.isoformat(' ', timespec='milliseconds')}'," \
+               f"f='{self.finish.isoformat(' ', timespec='milliseconds')}')"
+
+    @staticmethod
+    def trans_thread(thread: str):
+        return thread.replace('$', '').replace(r'\rightarrow', '->').replace('_', '').replace(' ', '')
+
 
 @dataclass
 class IFRRecord:
@@ -56,10 +71,14 @@ class IFRRecord:
     finish: datetime
     stages: List[Stage]
 
+    def __str__(self):
+        return f"IFRRecord(ifr={self.ifr_id}, start='{self.start.isoformat(' ', timespec='milliseconds')}', " \
+               f"finish='{self.finish.isoformat(' ', timespec='milliseconds')}', " \
+               f"stages={self.stages})"
 
-def read_ifr_records(m_tc: str, w_tcs: List[str], act2trd: Dict[Tuple[str, str], str]) -> List[IFRRecord]:
-    mi_evts = read_events(m_tc)
-    w_i_evts = [read_events(w_tc, len(mi_evts)) for w_tc in w_tcs]
+
+def events2records(mi_evts: List[List[Event]], w_i_evts: List[List[List[Event]]],
+                   act2trd: Dict[Tuple[str, str], str]) -> List[IFRRecord]:
     ircds = []
     for ifr_id in range(len(mi_evts)):
         m_evts = mi_evts[ifr_id]  # Master中当前IFR的所有事件
@@ -138,41 +157,82 @@ def show_ifr_records(ifr_records: List[IFRRecord], trds: List[str]):
     plt.show()
 
 
-if __name__ == '__main__':
-    REMOTE = True  # tc文件来源：True从远程下载，False从本地读取
+def read_from_zip(zip_name: str) -> Tuple[List[List[Event]], List[List[List[Event]]]]:
+    """从指定的zip文件中读取mi_evts, w_i_evts"""
+    with zipfile.ZipFile(zip_name) as tczip:
+        nworker = 1 + max(int(tcname.replace('worker', '').replace('.tc', ''))
+                          for tcname in tczip.namelist() if tcname.startswith('worker'))
+        with tczip.open('master.tc') as m_tcfile:
+            mi_evts = read_events(TextIOWrapper(m_tcfile))
+        w_i_evts = []
+        for wk in range(nworker):
+            with tczip.open(f'worker{wk}.tc') as w_tcfile:
+                w_i_evts.append(read_events(TextIOWrapper(w_tcfile), len(mi_evts)))
+        return mi_evts, w_i_evts
 
+
+def read_from_remote(device_cfg: str) -> Tuple[List[List[Event]], List[List[List[Event]]]]:
+    """根据device_cfg指定的device.yml，从远程服务器获取tc文件，并读取mi_evts, w_i_evts"""
     CACHE_DIR = 'tc-cache/'
     REMOTE_DIR = 'cnn-video/'
 
-    if REMOTE:
-        print("downloading tc from devices...")
-        if not os.path.isdir(CACHE_DIR):
-            os.mkdir(CACHE_DIR)
-        with open('device.yml', 'r', encoding='utf-8') as f:
-            config = yaml.load(f, yaml.Loader)
-        for role in config['role']:
-            devs = config['role'][role] if role == 'w' else [config['role']['m']]
-            for d, dev_name in enumerate(devs):
-                username = config['device'][dev_name]['user']
-                ip, port = config['device'][dev_name]['addr'].split(':')
-                tran = paramiko.Transport((ip, int(port)))
-                tran.connect(username=username, password=config['user'][username])
-                sftp = paramiko.SFTPClient.from_transport(tran)
-                remotedir = '/root/' if username == 'root' else f'/home/{username}/'
-                remotedir += REMOTE_DIR
-                filename = 'master.tc' if role == 'm' else f'worker{d}.tc'
-                sftp.get(remotedir + filename, CACHE_DIR + filename)
-        NWORKER = len(config['role']['w'])
-        tc_dir = CACHE_DIR
-    else:
-        with open('../config.yml', 'r', encoding='utf-8') as f:
-            config = yaml.load(f, yaml.Loader)
-        NWORKER = len(config['port']['worker'])
-        tc_dir = '..'
+    print("downloading tc from devices...")
+    if os.path.isdir(CACHE_DIR):
+        # 删除缓存目录，以确保缓存目录下都是最新的tc文件
+        shutil.rmtree(CACHE_DIR)
+    # 创建新的缓存目录
+    os.mkdir(CACHE_DIR)
+    with open(device_cfg, 'r', encoding='utf-8') as f:
+        config = yaml.load(f, yaml.Loader)
+    for role in config['role']:
+        devs = config['role'][role] if role == 'w' else [config['role']['m']]
+        for d, dev_name in enumerate(devs):
+            username = config['device'][dev_name]['user']
+            ip, port = config['device'][dev_name]['addr'].split(':')
+            tran = paramiko.Transport((ip, int(port)))
+            tran.connect(username=username, password=config['user'][username])
+            sftp = paramiko.SFTPClient.from_transport(tran)
+            remotedir = '/root/' if username == 'root' else f'/home/{username}/'
+            remotedir += REMOTE_DIR
+            filename = 'master.tc' if role == 'm' else f'worker{d}.tc'
+            sftp.get(remotedir + filename, CACHE_DIR + filename)
+    nworker = len(config['role']['w'])
+    mi_evts = read_events(f'{CACHE_DIR}/master.tc')
+    w_i_evts = [read_events(f'{CACHE_DIR}/worker{i}.tc', len(mi_evts)) for i in range(nworker)]
+    return mi_evts, w_i_evts
 
-    print("reading and ploting...")
+
+def read_from_local(local_dir: str) -> Tuple[List[List[Event]], List[List[List[Event]]]]:
+    """从指定的local_dir目录中寻找tc文件，读取mi_evts, w_i_evts"""
+    nworker = len(glob.glob(f'{local_dir}/worker*.tc'))  # 自动识别worker数量
+    mi_evts = read_events(f'{local_dir}/master.tc')
+    w_i_evts = [read_events(f'{local_dir}/worker{i}.tc', len(mi_evts)) for i in range(nworker)]
+    return mi_evts, w_i_evts
+
+
+if __name__ == '__main__':
+    # 3种模式：
+    #   l: local模式，从 LOCAL_DIR 指定的目录下寻找tc文件，根据目录下的文件名判断worker数
+    #   r: remote模式，从 REMOTE_CFG 获取远程服务器配置和worker数，下载远程tc文件
+    #   z: zip模式，从 TCZIP 指定的zip压缩包中读取tc文件，根据压缩包中的文件名判断worker数
+    MODE = 'r'
+
+    LOCAL_DIR = 'lbc2'  # l模式下, 本地目录路径
+    REMOTE_CFG = 'device.yml'  # 远程服务器的配置文件
+    TCZIP = 'edge.zip'  # 从zip文件中读取tc文件
+
+    if MODE == 'l':
+        g_mi_evts, g_w_i_evts = read_from_local(LOCAL_DIR)
+    elif MODE == 'r':
+        g_mi_evts, g_w_i_evts = read_from_remote(REMOTE_CFG)
+    elif MODE == 'z':
+        g_mi_evts, g_w_i_evts = read_from_zip(TCZIP)
+    else:
+        raise Exception(f"No such mode '{MODE}'")
+    print(f"events read succeeded, n_worker={len(g_w_i_evts)}, n_ifr={len(g_mi_evts)}")
+
     TRD2ACTS = {r'$m\rightarrow$': ['encode', 'transmit']}
-    for wid in range(NWORKER):
+    for wid in range(len(g_w_i_evts)):
         TRD2ACTS[rf'$\rightarrow w_{wid}$'] = ['decode']
         TRD2ACTS[f'$w_{wid}$'] = ['execute']
         TRD2ACTS[rf'$w_{wid}\rightarrow$'] = ['encode', 'transmit']
@@ -180,7 +240,9 @@ if __name__ == '__main__':
     for trd, acts in TRD2ACTS.items():
         for act in acts:
             ACT2TRD[trd.replace(r'\rightarrow', '').replace(' ', ''), act] = trd
-    g_ircds = read_ifr_records(f'{tc_dir}/master.tc', [f'{tc_dir}/worker{i}.tc' for i in range(NWORKER)], ACT2TRD)
+
+    print("transforming and ploting...")
+    g_ircds = events2records(g_mi_evts, g_w_i_evts, ACT2TRD)
     for ircd in g_ircds:
         print(ircd)
     show_ifr_records(g_ircds, list(TRD2ACTS.keys()))
